4bc17818ce027294da23f22ba1c43dd23acb613b
wangshangang <wangshangang@xender.com>
2018-07-10 11:08:29 +0800

    外部的回调接口

---
+++ transfer-core/src/main/java/cn/xender/core/server/utils/ActionListenerAdapter.java
@@ -0,0 +1,24 @@
+package cn.xender.core.server.utils;
+
+public abstract class ActionListenerAdapter implements ActionListener{
+
+    @Override
+    public void someoneOnline() {
+
+    }
+
+    @Override
+    public void someoneOffline() {
+
+    }
+
+    @Override
+    public void transferSuccess() {
+
+    }
+
+    @Override
+    public void transferFailure() {
+
+    }
+}

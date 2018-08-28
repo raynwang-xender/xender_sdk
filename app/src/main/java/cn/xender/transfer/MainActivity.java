9aadcb2021a5f4aaad4e82bfd11ac457e9f62741
Rayn Wang <jjdawangrui@163.com>
2018-08-28 09:58:49 +0800

    新建分支，欧阳vidmix

--- app/src/main/java/cn/xender/transfer/MainActivity.java
+++ app/src/main/java/cn/xender/transfer/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 
 import cn.xender.core.server.utils.NeedSharedFiles;
-import cn.xender.core.server.utils.NeedSharedFiles$FileItem;
 import cn.xender.transfertest.R;
 
 public class MainActivity extends Activity {
@@ -25,8 +24,10 @@ public class MainActivity extends Activity {
 
                 NeedSharedFiles.confirmStart();//初始化
                 NeedSharedFiles.setShareMyApk(true);//是否传输apk包，默认true
-                NeedSharedFiles.appendNewFile("/storage/emulated/0/aaa.png", NeedSharedFiles$FileItem.CATE_IMAGE);
-                NeedSharedFiles.appendNewFile("/storage/emulated/0/bbb.mp4", NeedSharedFiles$FileItem.CATE_VIDEO);
+//                NeedSharedFiles.appendNewFile("/storage/emulated/0/aaa.png", NeedSharedFiles$FileItem.CATE_IMAGE);
+//                NeedSharedFiles.appendNewFile("/storage/emulated/0/bbb.mp4", NeedSharedFiles$FileItem.CATE_VIDEO);
+                NeedSharedFiles.appendNewFile("/storage/emulated/0/aaa.png", NeedSharedFiles.FileItem.CATE_IMAGE);
+                NeedSharedFiles.appendNewFile("/storage/emulated/0/bbb.mp4", NeedSharedFiles.FileItem.CATE_VIDEO);
 
                 Intent intent = new Intent(MainActivity.this,ShareActivity.class);
                 startActivity(intent);
